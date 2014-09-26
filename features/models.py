3aad4153a0a17704c460be2b50d7baa6308a48eb
Seth Hill <seth@pointnineseven.com>
2014-09-25 17:16:05 -0700

    MP-260 #Comment Disable mapnik

--- features/models.py
+++ features/models.py
@@ -12,7 +12,7 @@ from manipulators.geometry import ensure_clean
 import logging
 from manipulators.manipulators import manipulatorsDict, NullManipulator
 import re
-import mapnik
+# import mapnik
 
 logger = logging.getLogger('features.models')
 
@@ -339,14 +339,14 @@ class SpatialFeature(Feature):
 
         return asKml(self.geometry_final, uid=self.uid)
 
-    @classmethod
-    def mapnik_style(self):
-        """
-        Mapnik style object containing rules for symbolizing features in staticmap
-        """
-        return None
-        style = mapnik.Style()
-        return style
+#     @classmethod
+#     def mapnik_style(self):
+#         """
+#         Mapnik style object containing rules for symbolizing features in staticmap
+#         """
+#         return None
+#         style = mapnik.Style()
+#         return style
 
     @property
     def kml(self):
@@ -515,18 +515,18 @@ class PolygonFeature(SpatialFeature):
         geom = self.geometry_final.point_on_surface.transform(GEOMETRY_CLIENT_SRID, clone=True)
         return geom.kml
 
-    @classmethod
-    def mapnik_style(self):
-        polygon_style = mapnik.Style()
-        ps = mapnik.PolygonSymbolizer(mapnik.Color('#ffffff'))
-        ps.fill_opacity = 0.5
-        ls = mapnik.LineSymbolizer(mapnik.Color('#555555'),0.75)
-        ls.stroke_opacity = 0.5
-        r = mapnik.Rule()
-        r.symbols.append(ps)
-        r.symbols.append(ls)
-        polygon_style.rules.append(r)
-        return polygon_style
+#     @classmethod
+#     def mapnik_style(self):
+#         polygon_style = mapnik.Style()
+#         ps = mapnik.PolygonSymbolizer(mapnik.Color('#ffffff'))
+#         ps.fill_opacity = 0.5
+#         ls = mapnik.LineSymbolizer(mapnik.Color('#555555'),0.75)
+#         ls.stroke_opacity = 0.5
+#         r = mapnik.Rule()
+#         r.symbols.append(ps)
+#         r.symbols.append(ls)
+#         polygon_style.rules.append(r)
+#         return polygon_style
 
     class Meta(Feature.Meta):
         abstract = True
@@ -545,18 +545,18 @@ class MultiPolygonFeature(SpatialFeature):
         geom = self.geometry_final.point_on_surface.transform(GEOMETRY_CLIENT_SRID, clone=True)
         return geom.kml
 
-    @classmethod
-    def mapnik_style(self):
-        polygon_style = mapnik.Style()
-        ps = mapnik.PolygonSymbolizer(mapnik.Color('#ffffff'))
-        ps.fill_opacity = 0.5
-        ls = mapnik.LineSymbolizer(mapnik.Color('#555555'),0.75)
-        ls.stroke_opacity = 0.5
-        r = mapnik.Rule()
-        r.symbols.append(ps)
-        r.symbols.append(ls)
-        polygon_style.rules.append(r)
-        return polygon_style
+#     @classmethod
+#     def mapnik_style(self):
+#         polygon_style = mapnik.Style()
+#         ps = mapnik.PolygonSymbolizer(mapnik.Color('#ffffff'))
+#         ps.fill_opacity = 0.5
+#         ls = mapnik.LineSymbolizer(mapnik.Color('#555555'),0.75)
+#         ls.stroke_opacity = 0.5
+#         r = mapnik.Rule()
+#         r.symbols.append(ps)
+#         r.symbols.append(ls)
+#         polygon_style.rules.append(r)
+#         return polygon_style
 
     class Meta(Feature.Meta):
         abstract = True
@@ -589,15 +589,15 @@ class LineFeature(SpatialFeature):
     geometry_final = models.LineStringField(srid=GEOMETRY_DB_SRID, 
             null=True, blank=True, verbose_name="Final LineString Geometry")
 
-    @classmethod
-    def mapnik_style(self):
-        line_style = mapnik.Style()
-        ls = mapnik.LineSymbolizer(mapnik.Color('#444444'),1.5)
-        ls.stroke_opacity = 0.5
-        r = mapnik.Rule()
-        r.symbols.append(ls)
-        line_style.rules.append(r)
-        return line_style
+#     @classmethod
+#     def mapnik_style(self):
+#         line_style = mapnik.Style()
+#         ls = mapnik.LineSymbolizer(mapnik.Color('#444444'),1.5)
+#         ls.stroke_opacity = 0.5
+#         r = mapnik.Rule()
+#         r.symbols.append(ls)
+#         line_style.rules.append(r)
+#         return line_style
 
     class Meta(Feature.Meta):
         abstract = True
@@ -630,13 +630,13 @@ class PointFeature(SpatialFeature):
     geometry_final = models.PointField(srid=GEOMETRY_DB_SRID, 
             null=True, blank=True, verbose_name="Final Point Geometry")
 
-    @classmethod
-    def mapnik_style(self):
-        point_style = mapnik.Style()
-        r = mapnik.Rule()
-        r.symbols.append(mapnik.PointSymbolizer())
-        point_style.rules.append(r)
-        return point_style
+#     @classmethod
+#     def mapnik_style(self):
+#         point_style = mapnik.Style()
+#         r = mapnik.Rule()
+#         r.symbols.append(mapnik.PointSymbolizer())
+#         point_style.rules.append(r)
+#         return point_style
 
     class Meta(Feature.Meta):
         abstract = True
