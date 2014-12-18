b787f8389e61f795c54421686fd57cce5331332f
Seth Hill <seth@pointnineseven.com>
2014-12-18 12:07:53 -0800

    MP-554 Remove the operator class hack. May not be needed in DJ17

--- features/migrations/0001_initial.py
+++ features/migrations/0001_initial.py
@@ -12,40 +12,6 @@ REVERSE_MARCO_ALBERS_SQL = '''
 delete from spatial_ref_sys where srid = 99996 limit 1;
 '''
 
-GIS_GEOMETRY_OPS_SQL = '''
-CREATE OPERATOR CLASS gist_geometry_ops
-FOR TYPE geometry USING GIST AS
-STORAGE box2df,
-OPERATOR        1        <<  ,
-OPERATOR        2        &<  ,
-OPERATOR        3        &&  ,
-OPERATOR        4        &>  ,
-OPERATOR        5        >>  ,
-OPERATOR        6        ~=  ,
-OPERATOR        7        ~   ,
-OPERATOR        8        @   ,
-OPERATOR        9        &<| ,
-OPERATOR        10       <<| ,
-OPERATOR        11       |>> ,
-OPERATOR        12       |&> ,
-
-OPERATOR        13       <-> FOR ORDER BY pg_catalog.float_ops,
-OPERATOR        14       <#> FOR ORDER BY pg_catalog.float_ops,
-FUNCTION        8        geometry_gist_distance_2d (internal, geometry, int4),
-
-FUNCTION        1        geometry_gist_consistent_2d (internal, geometry, int4),
-FUNCTION        2        geometry_gist_union_2d (bytea, internal),
-FUNCTION        3        geometry_gist_compress_2d (internal),
-FUNCTION        4        geometry_gist_decompress_2d (internal),
-FUNCTION        5        geometry_gist_penalty_2d (internal, internal, internal),
-FUNCTION        6        geometry_gist_picksplit_2d (internal, internal),
-FUNCTION        7        geometry_gist_same_2d (geom1 geometry, geom2 geometry, internal);
-'''
-
-REVERSE_GIS_GEOMETRY_OPS_SQL = '''
-drop operator class if exists gist_geometry_ops using gist;
-'''
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -53,5 +19,4 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(MARCO_ALBERS_SQL, REVERSE_MARCO_ALBERS_SQL),
-        migrations.RunSQL(GIS_GEOMETRY_OPS_SQL, REVERSE_GIS_GEOMETRY_OPS_SQL),
     ]
